6705a65506e88b1d8deb50b7c1ee73dc27b8d1ea
Brandon Nichols <brandonaronnichols@gamil.com>
2020-07-24 22:49:13 -0500

    completed ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -174,5 +174,59 @@ class Stack():
         return len(self.stack)
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    paths = [starting_node]
+    final_paths = []
+    largest_paths = []
+    ancestor_graph = {}
+    ancestor_stack = Stack()
+
+    ancestor_stack.push(paths)
+
+    for parent_child in ancestors:
+        ## in this for loop I am taking the child and using it as
+        ## the key, because I want the direction of the graph to be
+        ## pointed in the opossite direction and be able to tell
+        ## when i've finished a path by it having no neighbors
+        if parent_child[1] in ancestor_graph:
+            ancestor_graph[parent_child[1]].add(parent_child[0])
+        else:
+            ancestor_graph[parent_child[1]] = {parent_child[0]}
+
+    while ancestor_stack.size():
+        current_path = ancestor_stack.pop()
+        current_node = current_path[-1]
+
+        ## checks if current_node has neighbors
+        ## if it does not then it appends the
+        ## current_path to the list of final_paths
+        ## else it gets the neighbors fo the current_node
+        ## and pushes the neighbors new_paths to the stack
+        if current_node not in ancestor_graph:
+            final_paths.append(current_path)
+        else:
+            neighbors = ancestor_graph[current_node]
+            for neighbor in neighbors:
+                new_path = current_path[:]
+                new_path.append(neighbor)
+                ancestor_stack.push(new_path)
+
+    if len(final_paths[0]) == 1:
+        return -1
+
+    for path in final_paths:
+        if len(largest_paths) == 0:
+            largest_paths.append(path)
+        else:
+            if len(path) > len(largest_paths[0]):
+                largest_paths = []
+                largest_paths.append(path)
+            elif len(path) == len(largest_paths[0]):
+                largest_paths.append(path)
+
+    min_value = largest_paths[0][-1]
+    for path in largest_paths:
+        if path[-1] < min_value:
+            min_value = path[-1]
+        
+    return min_value
 # Review
